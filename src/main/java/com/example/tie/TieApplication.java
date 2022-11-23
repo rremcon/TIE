865bd4a11ea096f90d69919b1e5eaf897513c597
rremcon <rremcon@outlook.com>
2022-11-23 12:19:29 +0100

    first commit

---
+++ src/main/java/com/example/tie/TieApplication.java
@@ -0,0 +1,13 @@
+package com.example.tie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TieApplication.class, args);
+    }
+
+}
